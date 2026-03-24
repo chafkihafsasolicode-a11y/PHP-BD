d9ac40a2169f76d1bd85c7aee08c02112f7fd7bb
https://github.com/chafkihafsasolicode-a11y <chafkihafsa.solicode@gmail.com>
2026-03-24 09:29:12 +0000

    chapitre1

--- Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
+++ Chapitre 1 – Connexion à une base de données avec PDO/connexion.php
@@ -1,15 +1,19 @@
-CREATE DATABASE blogdb;
 <?php
 $host = 'localhost';
 $dbname = 'blogdb';
 $username = 'root';
 $password = '';
 
-try {
+try{
+
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connexion réussie à la base $dbname";
-} catch (PDOException $e) {
+}
+catch(PDOException $e){
     echo "Erreur de connexion : " . $e->getMessage();
 }
+
+
+
 ?>
